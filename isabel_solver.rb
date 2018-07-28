d1509da06f2b53c9725e0f8ed5dcd95eee140cf0
takano7 <41814149+takano7@users.noreply.github.com>
2018-07-28 22:33:49 +0900

    Add function getLineSubset()
    You can use getLineSubset() to determine the subsets which consists the edge of the square.

--- isabel_solver.rb
+++ isabel_solver.rb
@@ -13,9 +13,31 @@ def getSet( s, x=1 )
   getSet( s-x2, x+1 ).each { |a| a.unshift x }.concat getSet( s, x+1 )
 end
 
+# Get subsets of arr to divide length len
+#
+# [IN ] arr : natural number set
+# [IN ] len : The length to be divided into elements of subset
+# [Ret] Two-dimensional array. Set of subsets of arr to divide length len.
+def getLineSubset( arr, len )
+  return [] if (arr.empty? || (x = arr[0]) > len)
+  return [[x]] if x == len
+  ar = arr.drop(1)
+  getLineSubset( ar, len-x )
+    .each{ |a| a.unshift x }
+    .concat getLineSubset( ar, len )
+end
+
 # Output the set of square sets dividing the area size inputed.
-s = gets.to_i
-while s > 0 do
-  getSet(s).each{ |a| p a }
-  s = gets.to_i
+print 'Input the area size of gold bar : '
+gold = gets.to_i
+print 'Input the size of square : '
+len = gets.to_i
+while (s = len**2 - gold) > 0 do
+  (arr = getSet(s)).each { |a| p a }
+
+  print 'Input the index above : '
+  getLineSubset(arr[gets.to_i % arr.length], len).each{ |a| p a }
+
+  print 'Input the size of square : '
+  len = gets.to_i
 end
